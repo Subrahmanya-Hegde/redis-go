006f87ea5449fe20df94462474b05a133650def2
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2026-04-12 21:56:52 +0530

    feat | handle concurrent clients and added RESP parser for supporting multiple commands

--- app/main.go
+++ app/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"net"
 	"os"
+	"strconv"
+)
+
+const (
+	BulkString = '$'
+	Array      = '*'
 )
 
 var _ = net.Listen
 var _ = os.Exit
+var ErrLineTooShort = errors.New("line too short")
+var ErrorResponse = "-ERR %s\n"
+var SuccessResponse = "+%s\r\n"
 
 func main() {
 	fmt.Println("Local: http://0.0.0.0:6379")
@@ -31,18 +42,130 @@ func main() {
 }
 
 func handleConnection(connection net.Conn) {
-	defer connection.Close()
+	defer func(connection net.Conn) {
+		err := connection.Close()
+		if err != nil {
+			fmt.Printf("Error while closing connection: %s\n", err.Error())
+		}
+	}(connection)
+	reader := bufio.NewReader(connection)
+
 	for {
-		buffer := make([]byte, 1024)
-		_, err := connection.Read(buffer)
+		commands, err := getCommandsArray(reader)
 		if err != nil {
 			if err == io.EOF {
-				fmt.Println("Client disconnected")
-				break
+				fmt.Print("client disconnected.")
+				return
 			}
-			fmt.Println("Error while reading from connection.", err.Error())
-			os.Exit(1)
+			writeToConnection(connection, fmt.Sprintf(ErrorResponse, err.Error()))
+			return
 		}
-		connection.Write([]byte("+PONG\r\n"))
+		commandsLength := len(commands)
+		if commandsLength == 0 {
+			fmt.Println("No commands received, closing connection.")
+			return
+		}
+		if commandsLength > 2 {
+			writeToConnection(connection, fmt.Sprintf(ErrorResponse, "more than 2 commands received."))
+			return
+		}
+
+		if commandsLength == 1 {
+			if commands[0] != "PING" {
+				writeToConnection(connection, fmt.Sprintf(ErrorResponse, "unrecognized command"))
+				return
+			}
+			writeToConnection(connection, fmt.Sprintf(SuccessResponse, "PONG"))
+			return
+		}
+		if commands[0] != "ECHO" {
+			writeToConnection(connection, fmt.Sprintf(ErrorResponse, "unrecognized command."))
+			return
+		}
+		writeToConnection(connection, fmt.Sprintf(SuccessResponse, commands[1]))
 	}
+}
+
+func writeToConnection(connection net.Conn, line string) {
+	_, _ = connection.Write([]byte(line))
+}
+
+// Sample RESP (REdis Serialization Protocol): *2\r\n$3\r\nGET\r\n$3\r\nkey\r\n
+// * for array, $ for bulk string, + for simple string, : for integer, - for error
+func getCommandsArray(reader *bufio.Reader) ([]string, error) {
+	fmt.Println("Reading commands from connection...")
+
+	firstByte, err := reader.ReadByte()
+	var commands []string
+	if err != nil {
+		if err == io.EOF {
+			return commands, err
+		}
+		fmt.Printf("Error while reading first byte: %s\n", err.Error())
+		return commands, err
+	}
+	if firstByte != Array {
+		return commands, fmt.Errorf("expected array, got %c", firstByte)
+	}
+	line, err := readLine(reader)
+	if err != nil {
+		return commands, err
+	}
+	commandsLength, err := strconv.Atoi(line)
+	if err != nil {
+		return commands, err
+	}
+	commands = make([]string, commandsLength)
+
+	for i := range commandsLength {
+		command, err := readBulkString(reader)
+		if err != nil {
+			return commands, err
+		}
+		commands[i] = command
+	}
+	return commands, nil
+}
+
+func readBulkString(reader *bufio.Reader) (string, error) {
+	bulkStringByte, err := reader.ReadByte()
+	if err != nil {
+		return "", err
+	}
+	if bulkStringByte != BulkString {
+		return "", fmt.Errorf("expected bulk string, got %c", bulkStringByte)
+	}
+	line, err := readLine(reader)
+	if err != nil {
+		return "", err
+	}
+	bulkStringLength, err := strconv.Atoi(line)
+	if err != nil {
+		return "", err
+	}
+	bulkStringBytes := make([]byte, bulkStringLength)
+	_, err = io.ReadFull(reader, bulkStringBytes)
+	if err != nil {
+		return "", err
+	}
+	_, err = readLine(reader)
+	if err != nil {
+		if errors.Is(err, ErrLineTooShort) {
+			return "", fmt.Errorf(fmt.Sprintf("expected at least %d bytes.", bulkStringLength))
+		}
+		return "", err
+	}
+	return string(bulkStringBytes), nil
+}
+
+func readLine(reader *bufio.Reader) (string, error) {
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return "", fmt.Errorf("error reading line: %s", err.Error())
+	}
+	lineLength := len(line)
+	if lineLength < 2 {
+		return "-ERR", ErrLineTooShort
+	}
+	return line[:lineLength-2], nil //Line without \r\n
 }
