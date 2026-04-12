c3bb4d03569fe00057d9cb4d8d4561a1a89e850c
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2026-04-12 21:51:03 +0530

    feat | respond to ping

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -10,5 +11,38 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	fmt.Println("Redis in go project init ...")
+	fmt.Println("Local: http://0.0.0.0:6379")
+
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	defer listener.Close()
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error while accepting the connection.", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(connection)
+	}
+}
+
+func handleConnection(connection net.Conn) {
+	defer connection.Close()
+	for {
+		buffer := make([]byte, 1024)
+		_, err := connection.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Client disconnected")
+				break
+			}
+			fmt.Println("Error while reading from connection.", err.Error())
+			os.Exit(1)
+		}
+		connection.Write([]byte("+PONG\r\n"))
+	}
 }
